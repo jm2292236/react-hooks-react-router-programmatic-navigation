4465d7a9a93e11cc29ad471d4f1a30672577a4c4
Jaime Montes de Oca <jaime0422@live.com>
2022-06-19 19:51:50 -0700

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,24 +6,28 @@ import Login from "./Login";
 import Navbar from "./Navbar";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userName, setUserName] = useState("");
 
-  return (
-    <div>
-      <Navbar setIsLoggedIn={setIsLoggedIn} />
-      <Switch>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/login">
-          <Login setIsLoggedIn={setIsLoggedIn} />
-        </Route>
-        <Route exact path="/">
-          <Home isLoggedIn={isLoggedIn} />
-        </Route>
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar setIsLoggedIn={setIsLoggedIn} userName={userName} setUserName={setUserName} />
+            
+            <Switch>
+                <Route exact path="/about">
+                    <About />
+                </Route>
+
+                <Route exact path="/login">
+                    <Login setIsLoggedIn={setIsLoggedIn} setUserName={setUserName} />
+                </Route>
+
+                <Route exact path="/">
+                    <Home isLoggedIn={isLoggedIn} />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,50 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-function Login({ setIsLoggedIn }) {
-  const history = useHistory();
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-
-  function handleChange(e) {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
+function Login({ setIsLoggedIn, setUserName }) {
+    const history = useHistory();
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
     });
-  }
 
-  function handleSubmit(e) {
-    e.preventDefault();
+    function handleChange(e) {
+        setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+        });
+    }
 
-    setIsLoggedIn(true);
+    function handleSubmit(e) {
+        e.preventDefault();
 
-    // after logging the user in, redirect to the home page!
-    history.push("/");
-  }
+        setIsLoggedIn(true);
+        setUserName(formData.username);
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
-      <input
-        type="text"
-        name="username"
-        value={formData.username}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-      />
-      <button type="submit">Login</button>
-    </form>
-  );
+        // after logging the user in, redirect to the home page!
+        history.push("/");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <input
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+            />
+
+            <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+            />
+
+            <button type="submit">Login</button>
+        </form>
+    );
 }
 
 export default Login;
